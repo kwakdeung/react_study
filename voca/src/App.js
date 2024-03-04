445ff82bdf1499baa858f86daba4b1c11dd92624
kwakdeung <rhkrwnsdud22@naver.com>
2024-03-04 18:00:51 +0900

    React JS #6 이벤트 처리(Handling Events) - 초보자를 위한 리액트 강좌

--- voca/src/App.js
+++ voca/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Hello from "./component/Hello";
-import Welcome from "./component/Welcome";
 import styles from "./App.module.css"
 
 
@@ -8,7 +7,6 @@ function App() {
   return (
   <div className="App">
     <Hello />
-    <div className={styles.box}>App</div>
   </div>
   );
 }
