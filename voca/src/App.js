ef1ec7a558c5a17debbb7711fb46ef49fd5cd76b
kwakdeung <rhkrwnsdud22@naver.com>
2024-03-04 17:39:24 +0900

    React JS #3 컴포넌트, JSX - 초보자를 위한 리액트 강좌

--- voca/src/App.js
+++ voca/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
-import Hello from './component/Hello';
 
 
 function App() {
-  return <div className="App">
-    <Hello />
-  </div>;
+  const name = "Tom";
+  const naver = {
+    name: "네이버",
+    url: "https://naver.com",
+  };
+  return (
+  <div className="App">
+    <h1
+      style={{
+        color: "#f0f",
+        backgroundColor: "green",
+      }}
+    >
+      Hello, {name}.<p>{2 * 3}</p>
+    </h1>
+    <a href={naver.url}>{naver.name}</a>
+  </div>
+  );
 }
 
 export default App;
