e95c824812d7e4aa1e01479c2d9598162c46dc0f
kwakdeung <rhkrwnsdud22@naver.com>
2024-03-05 15:21:14 +0900

    React JS #8 props - 초보자를 위한 리액트 강좌

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -1,17 +1,19 @@
 import { useState } from 'react';
+import Username from './Username';
 
-export default function Hello() {
-    // let name = "Mike";
+export default function Hello({ age }) {
+    
     const [name, setName] = useState('Mike');
+    const msg = age > 19 ? "성인 입니다.": "미성년자 입니다.";
 
-    function changeName() {
-        setName(name === "Mike" ? "Jane" : "Mike");
-    }
 
     return (
         <div>        
-            <h2 id="name">{name}</h2>           
-            <button onClick={changeName}>Change</button>
+            <h2 id="name">{name}({age}) : {msg}</h2>
+            <Username name={name} />
+            <button onClick={() => {
+                setName(name === "Mike" ? "Jane" : "Mike");
+            }}>Change</button>
         </div>
     );
 }
